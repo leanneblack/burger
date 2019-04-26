1f21fcaec67a094d4568946a7c0206c1dbb9f958
leanneblack <leanne.black@cox.net>
2019-04-26 09:42:14 -0700

    Initial Push
    styling and some functionality

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burgerName) VALUES ('Impossible Burger');
+INSERT INTO burgers (burgerName) VALUES ('In N Out Hamburger');
+INSERT INTO burgers (burgerName, devoured) VALUES ('In N Out Double Double', true);
+INSERT INTO burgers (burgerName, devoured) VALUES ('Super Deluxe Burger', true);
+INSERT INTO burgers (burgerName, devoured) VALUES ('McChicken', false);
+
